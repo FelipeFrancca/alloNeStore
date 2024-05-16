a761982bf9aab6dd735be1e06419fd14b7891b62
Felipe.Francca <dev.felipefrancca@gmail.com>
2024-05-15 22:00:20 -0300

    up

--- src/pages/dashboard/views/home.jsx
+++ src/pages/dashboard/views/home.jsx
@@ -1,20 +1,82 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, IconButton, Tooltip, Typography, Zoom } from "@mui/material";
 import "../../../assets/styles/home.css";
 
-import Headphones from "../../../assets/images/headphones.png"
+import Headphones from "../../../assets/images/headphones.png";
 
 export default function Home() {
   return (
     <Box className="container">
-      <Box className="container">
-        <Box className="topic1">
-          <Typography className="topic1Text1">Atendemos todas as suas demandas</Typography>
-          <Typography className="topic1Text2">Os melhores produtos custo beneficio do mercado!</Typography>
+      <Box className="containerInicio1">
+        <Box className="containerInicio2">
+          <Box className="topic1">
+            <Typography className="topic1Text1">
+              Atendemos todas as suas demandas
+            </Typography>
+            <Typography className="topic1Text2">
+              Os melhores produtos custo beneficio do mercado!
+            </Typography>
+            <Box className="miniMenu">
+              <Tooltip
+                title="Ir para fones de ouvido"
+                placement="bottom"
+                TransitionComponent={Zoom}
+                sx={{
+                  borderRadius: "50px",
+                  padding: "5px",
+                  border: "solid 1px #fff",
+                }}
+              >
+                <IconButton href="#contato">
+                  <Typography color="#fff" fontSize={20} fontWeight={900}>
+                    Fones
+                  </Typography>
+                </IconButton>
+              </Tooltip>
+              <Tooltip
+                title="Ir para Relógios"
+                placement="bottom"
+                TransitionComponent={Zoom}
+                sx={{
+                  borderRadius: "50px",
+                  padding: "5px",
+                  border: "solid 1px #fff",
+                }}
+              >
+                <IconButton href="#contato">
+                  <Typography color="#fff" fontSize={20} fontWeight={900}>
+                    Relógios
+                  </Typography>
+                </IconButton>
+              </Tooltip>
+              <Tooltip
+                title="Ir para Bijuterias"
+                placement="bottom"
+                TransitionComponent={Zoom}
+                sx={{
+                  borderRadius: "50px",
+                  padding: "5px",
+                  border: "solid 1px #fff",
+                }}
+              >
+                <IconButton href="#contato">
+                  <Typography color="#fff" fontSize={20} fontWeight={900}>
+                    Bijuterias
+                  </Typography>
+                </IconButton>
+              </Tooltip>
+            </Box>
+          </Box>
+
+          <Box className="topic2">
+            <img src={Headphones} alt="Fones" />
+          </Box>
         </Box>
-        <Box className="topic2">
-          <img src={Headphones} alt="Fones" />
-        </Box>
+      </Box>
+      <Box>
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, odit? Laudantium numquam eum eos, sint excepturi, ab atque vel dolores eaque aliquid omnis ut, quos fuga expedita quae! Odio odit repellat ut animi temporibus quod libero ducimus doloremque placeat vitae repudiandae vel aliquam culpa exercitationem necessitatibus itaque, facilis magni eum commodi modi ea illo eligendi. Ad, officia. Voluptas nam cum sed minus consequuntur quae numquam impedit aliquam autem facilis quaerat id officia, ut quod dolore pariatur maxime tempora inventore! Officia tempora deserunt, nihil minus illo ipsam itaque mollitia voluptatem? Maiores asperiores, at quos eaque provident sit labore quaerat quod. Dolorum blanditiis, possimus porro nobis error in sapiente sit expedita repellendus debitis ipsa ad quas consectetur hic iusto natus ullam corrupti animi totam. Adipisci suscipit quia vero, voluptatem distinctio asperiores eveniet ab error. Tempora excepturi obcaecati recusandae optio modi placeat ducimus nam expedita? Ratione quo veritatis labore itaque beatae obcaecati ex quos illo. Soluta sint repudiandae harum, voluptatibus facere, nostrum molestiae praesentium et necessitatibus enim, eum voluptatum quidem tenetur adipisci pariatur ipsa dignissimos corrupti totam. Libero eaque fugit hic minus temporibus possimus aut earum sapiente facilis officia est, repellat ad tenetur mollitia, delectus non esse quasi nihil sunt incidunt, dicta unde nisi adipisci quae. Repellat ducimus explicabo modi beatae. Alias rerum voluptatem assumenda voluptas doloremque repellat, sed iusto velit autem impedit, pariatur saepe? Officia illo beatae voluptatem dolorem, eius sit aperiam ab non! Totam vero id repellat quibusdam perferendis unde maxime culpa a tenetur sapiente quisquam ea necessitatibus dicta ab, nostrum voluptatum ad adipisci. Aliquid, magni. Cum dolorum repellat eligendi quia doloribus veritatis fugiat beatae, corporis dolores consectetur ullam culpa quisquam at delectus nam hic minima tempore quod, unde sunt error facere ipsa harum? Minima consectetur laboriosam atque! Accusamus unde soluta sit. Dolore facere soluta laboriosam saepe explicabo nostrum aspernatur commodi et sequi, iusto praesentium sed nesciunt quod qui voluptas vitae optio, ipsam architecto quae repellat officia possimus minima consequatur? Consequuntur magnam nemo reiciendis minima, sit, nostrum impedit consequatur laudantium adipisci minus maiores aut rem blanditiis! Quo alias tempore molestiae laboriosam vitae perspiciatis animi blanditiis iusto dicta laudantium possimus dolore delectus, veniam illum aut fuga ipsa. Libero et repellendus itaque magni magnam ducimus quia, tenetur, perferendis obcaecati, sed praesentium modi deleniti odio facilis tempora sint! Beatae hic nobis aperiam, corrupti maiores modi ad minus iste omnis. Quaerat unde numquam quo! Delectus quos possimus beatae vel in iure eum consectetur a iusto porro, vitae odit omnis ratione id alias dicta? Eius voluptatibus unde facilis consectetur ratione eum quidem minima, velit quisquam eaque modi corporis quis ab repellat ipsam illo ipsum? Alias veritatis quod aperiam tempore id eligendi a repellat, voluptatem ea sunt pariatur repudiandae autem, odio eos excepturi quisquam placeat porro. Possimus sint perspiciatis voluptate ab atque laudantium, totam earum voluptatibus nemo commodi similique a fugiat aut illum sed id qui amet consectetur aliquam? Adipisci molestiae tempore sit quidem aliquid beatae aut ratione eaque ipsum soluta laborum tenetur ipsa, ab voluptate debitis, at natus. Possimus aperiam labore optio, veritatis voluptate rerum autem accusamus fugiat, dignissimos incidunt eligendi voluptatibus in molestiae ratione? Aspernatur non numquam corrupti molestiae quas! Iure id quibusdam excepturi laborum aspernatur. Recusandae reprehenderit magnam temporibus laborum natus unde numquam maxime cum quis ex, dignissimos voluptates, nihil est quae eligendi quas tempore sit provident. Eos adipisci dolorum explicabo excepturi ex reiciendis. Cupiditate consequuntur quas tempore assumenda? Nostrum iusto culpa et est accusantium reiciendis aliquid blanditiis alias iure. Est ea animi a temporibus maxime. Repellat delectus eius sit ducimus et? Animi recusandae aliquid veniam vitae eum, illum eos nulla doloribus iure, asperiores, cumque consequatur qui. Incidunt, eum beatae possimus dolorem dolore voluptatibus dolores voluptates minima fugiat, aspernatur cupiditate dicta labore eius. Est cum neque ipsa nemo, aspernatur sunt ad adipisci voluptatem voluptatum, numquam repellendus ipsum voluptate libero quos impedit praesentium illum, temporibus laudantium aut debitis facere? Ipsam ea tempore cum sed quisquam expedita exercitationem architecto, magni deserunt accusantium. Ullam tenetur, rem quaerat a cupiditate minus earum aut dignissimos veniam minima distinctio hic quod eaque id modi! Molestiae, iste rerum. Similique architecto tenetur laudantium blanditiis. Nostrum ipsam dicta saepe error modi itaque ea rerum provident fuga voluptatum distinctio ducimus eligendi laudantium, odit quisquam! Consequuntur sit, porro vitae distinctio, totam cum odio, vero quisquam veritatis alias repellat blanditiis provident modi deserunt. Alias tempora rerum deleniti maiores ea ad dolore perspiciatis, accusamus architecto, deserunt non, iusto temporibus? Reiciendis cum ipsa, quidem iusto minus illo modi sit molestias voluptates quia perspiciatis aut, quisquam magnam ratione dolores eveniet aspernatur labore ea. Voluptatum obcaecati quisquam nobis? Exercitationem at nisi omnis libero vero adipisci. Animi quod id, perspiciatis voluptates debitis voluptatum a explicabo necessitatibus inventore atque provident deserunt ab reprehenderit rem dolorum error aliquam corrupti adipisci soluta voluptate quibusdam facilis magni ad iure. Corrupti similique autem quod recusandae pariatur consectetur adipisci ducimus iste, exercitationem dolorem quam alias mollitia nam debitis at iusto? Quam deleniti quos architecto laborum optio quidem vel et dolores eveniet unde harum hic, quibusdam nam! Repudiandae in voluptatem, blanditiis adipisci veritatis aspernatur itaque magni aliquid. Ex quasi et explicabo obcaecati architecto temporibus, tempore perferendis labore fugit magni atque consequatur corrupti sunt quas quis repudiandae velit dicta accusantium quia voluptatibus voluptates. Sit dolores autem qui nemo eos nostrum non, sapiente unde nobis iusto repudiandae doloremque dicta itaque odio aut debitis placeat modi. Atque harum molestiae alias illo numquam laborum nisi debitis maxime recusandae aperiam provident possimus error sunt similique nemo consectetur perspiciatis pariatur distinctio odio delectus enim, eaque voluptatem nulla ea. Consectetur magnam accusantium voluptatem tempora eius saepe. Accusamus non nemo ullam, vitae laborum perspiciatis laboriosam. Cupiditate inventore reprehenderit neque consequuntur asperiores, sapiente possimus blanditiis numquam officia ut nihil fugit optio non cum, similique soluta laborum atque perspiciatis, rerum aut aspernatur cumque molestiae recusandae fugiat! Nihil, quod porro repellendus optio, temporibus eius itaque distinctio harum, autem sint velit cumque. Consectetur maiores vitae ipsum non molestias earum? Earum ab ratione voluptates facilis neque pariatur quo itaque possimus aliquid quod aperiam repudiandae vel nihil dignissimos, rem iure. Deleniti voluptate necessitatibus inventore quis cum quibusdam ex voluptatem possimus, consequuntur eveniet!
+        </Typography>
       </Box>
     </Box>
   );
